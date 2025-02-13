49ad9ee913c214e8406b7b121a75243202d6a260
David Breckx <david.breckx@telenet.be>
2025-02-13 20:16:07 +0100

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, useAnimatedValue, View } from "react-native";
+import Header from "./src/components/Header";
+import { useState } from "react";
+import Counter from "./src/components/Counter";
 
 export default function App() {
+  let title = "Mobile Les 1";
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Header title={title} />
+      <Counter counter={counter} setCounter={setCounter} />
+      <Text>Hello Mobile!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
